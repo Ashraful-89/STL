816e7a8dfa66d6b1edcb41fb869737d16feeb06f
MD : ASHRAFUL ISLAM <97118095+Ashraful-89@users.noreply.github.com>
2023-01-09 22:41:12 +0600

    Create input_output.cpp

---
+++ String/input_output.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  string s;
+  cout << "Enter the String" << endl; // take a string
+  cin >> s;
+  
+  cout << "The string is : " << s << endl;
+  return 0;
+}
